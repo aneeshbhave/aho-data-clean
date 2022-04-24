636a568ea4f40b53647625f0fd9c7744d3454d92
Aneesh Bhave <aneesh1701@gmail.com>
2022-04-24 16:39:10 +0530

    :q!

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-import ahocorasick, random
+import ahocorasick, json
 
+def Main(request):
+    dict = ["aneesh", "bhave", "archis"]
+    pool = "aneesh archis bhave"
+    MatchAho(dict, pool)
 
-def CleanData(data : str) -> str:
+def Clean(data :str) -> str:
     return data.replace(' ', '').replace('-', '')[-10:]
 
-def MatchText(pool : str, dict : list) -> list:
-    a = ahocorasick.Automaton()
-    i = 0
-    for element in dict:
-        a.add_word(element, (i, element))
-        i += 1
-    a.make_automaton()
-    for endIdx, (insertOrder, originalValue) in a.iter(pool):
-        startIdx = endIdx - len(originalValue) + -1
-        print((startIdx, endIdx), (insertOrder, originalValue))
+def MatchAho(dict :list, pool :str):
+    aho = ahocorasick.Automaton()
+    
+    for i in range(len(dict)):
+        aho.add_word(dict[i], (i, dict[i]))
 
+    
+    aho.make_automaton()
 
-dict = open("./dict.txt", 'r')
-arr = dict.read().splitlines()
+    print(pool)
+    for j, (idx, originalValue) in aho.iter(pool):
+        i = j - len(originalValue) + 1
+        print(f"{originalValue} from {i} to {j} with insert order of {idx}")
+        # assert pool[i:i + len(originalValue)] == originalValue
 
-pool = open("./pool.txt", 'r')
-poolText = pool.read()
-
-MatchText(poolText, arr)
-
-
+if __name__ == "__main__":
+    Main(None)
