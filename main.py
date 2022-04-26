405b10bed1a670e6d143142e1dad6e197ff39d37
Aneesh Bhave <aneesh1701@gmail.com>
2022-04-26 23:04:43 +0530

    :q

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 """
 ------------------------------------------------------------------------------
 ----------------------------------TODO TABLE----------------------------------
+Code -->
+[ ] Convert Main() to request/response format
+[X] Make Clean() use a regular expression NOTE: UNNECESSARY OPTIMIZATION
+[ ] Make MatchAho() return an array of JS Object instead of a list of dicts
 
 Cloud -->
-[ ] Convert Main() to request/response format
 [ ] Make sure function works when called in a script
 
 Debugging -->
-[ ] Test function on more test cases 
+[*] Test function on more test cases 
 [ ] CHECK FOR ERRORS IN EVERY POSSIBLE CREVICE
 
 Documentation -->
-[ ] Write documentation for code in github repo
-[ ] Comment code before you lose sanity
+[*] Write documentation for code in github repo
+[*] Comment code before you lose sanity
 ------------------------------------------------------------------------------
 """
 
@@ -30,25 +33,29 @@ def Main(request):
     pool = filePool.read()
 
     print(MatchAho(dict, pool))
-    
+
     filePool.close()
     fileDict.close()
 
+#Clean phone number data and return it in a format of 10 digits
 def Clean(data :str) -> str:
     return data.replace(' ', '').replace('-', '')[-10:]
 
+#Create trie from dict and search in pool, key is used to access phone number in JSON
 def MatchAho(dict :list, pool :str, key :str = "number") -> list:
     aho = ahocorasick.Automaton()
-    common = []
+    common = [] #List of dicts containing common contacts
 
+    #First loop to insert elements to trie
     insertIndex = 0;
     for obj in dict:
         cleanData = Clean(obj[key])
         aho.add_word(cleanData, (insertIndex, cleanData))
         insertIndex += 1
     
-    aho.make_automaton()
-
+    aho.make_automaton() #Build output and suffix links
+    
+    #Second loop to search through pool using trie
     for j, (idx, originalValue) in aho.iter(pool):
         i = j - len(originalValue) + 1
         common.append(dict[idx])
@@ -56,6 +63,6 @@ def MatchAho(dict :list, pool :str, key :str = "number") -> list:
     
     return common
 
-#!REMOVE BELOW LINES BEFORE DEPLOYMENT
+#WARN: REMOVE BELOW LINES BEFORE DEPLOYMENT
 if __name__ == "__main__":
     Main(None)
